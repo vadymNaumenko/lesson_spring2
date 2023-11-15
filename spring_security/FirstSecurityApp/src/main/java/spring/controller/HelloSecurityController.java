e6e083ab2da78f0410e5e6885eb19db28f3943e2
Vdym <vadimnaumnaum@gmail.com>
2023-11-15 17:26:49 +0100

    @PreAuthorize  admin

--- spring_security/FirstSecurityApp/src/main/java/spring/controller/HelloSecurityController.java
+++ spring_security/FirstSecurityApp/src/main/java/spring/controller/HelloSecurityController.java
@@ -1,15 +1,18 @@
 package spring.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import spring.entity.Person;
 import spring.security.PersonDetails;
+import spring.service.AdminService;
 
 @Controller
 public class HelloSecurityController {
-
+@Autowired
+    private AdminService adminService;
     @GetMapping("/hello")
     public String helloSecurity(){
         return "./auth/hello";
@@ -25,5 +28,13 @@ public class HelloSecurityController {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         PersonDetails personDetails = (PersonDetails) authentication.getPrincipal();
         return personDetails.getPerson();
+    }
+
+    @GetMapping("/admin")
+    public String admin(){
+
+        adminService.doAdminStaff();
+
+        return "admin";
     }
 }
