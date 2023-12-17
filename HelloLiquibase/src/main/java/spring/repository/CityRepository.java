b0883f49460923b27f23fcf9396e9db02c8ed5a3
Vdym <vadimnaumnaum@gmail.com>
2023-12-17 22:58:13 +0100

    Seting for deploy

--- HelloLiquibase/src/main/java/spring/repository/CityRepository.java
+++ HelloLiquibase/src/main/java/spring/repository/CityRepository.java
@@ -1,11 +1,13 @@
 package spring.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import spring.domin.City;
 
 @Repository
 public interface CityRepository extends JpaRepository<City,Long> {
 
+    @Query("select c from City c where c.name like :name")
     City findByName(String name);
 }
