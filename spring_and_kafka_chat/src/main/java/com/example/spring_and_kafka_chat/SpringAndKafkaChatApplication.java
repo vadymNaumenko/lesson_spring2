f6080ae7c8c98de752e1dfb1411ef9e7f379a4e2
Vdym <vadimnaumnaum@gmail.com>
2024-03-06 22:19:28 +0100

    kafka chat

---
+++ spring_and_kafka_chat/src/main/java/com/example/spring_and_kafka_chat/SpringAndKafkaChatApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_and_kafka_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAndKafkaChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAndKafkaChatApplication.class, args);
+    }
+
+}
---
+++ spring_and_kafka_chat/src/test/java/com/example/spring_and_kafka_chat/SpringAndKafkaChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_and_kafka_chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAndKafkaChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
