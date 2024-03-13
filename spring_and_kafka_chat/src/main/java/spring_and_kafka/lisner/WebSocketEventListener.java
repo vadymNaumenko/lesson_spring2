38e69588e4945b61c49c08486a00f697369f30c0
Vdym <vadimnaumnaum@gmail.com>
2024-03-13 09:41:58 +0100

    created  WebSocketEventListener

---
+++ spring_and_kafka_chat/src/main/java/spring_and_kafka/lisner/WebSocketEventListener.java
@@ -0,0 +1,41 @@
+package spring_and_kafka.lisner;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionConnectedEvent;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+import spring_and_kafka.entity.Message;
+import spring_and_kafka.entity.type.MessageType;
+
+@Component
+@Slf4j
+public class WebSocketEventListener {
+
+    private final SimpMessageSendingOperations messagingTemplate;
+
+    public WebSocketEventListener(SimpMessageSendingOperations messagingTemplate) {
+        this.messagingTemplate = messagingTemplate;
+    }
+
+    @EventListener
+    public void handleWebSocketConnectListener(SessionConnectedEvent event) {
+        log.info("Received a new web socket connection");
+    }
+
+    @EventListener
+    public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String username = (String) headerAccessor.getSessionAttributes().get("username");
+
+        if (username != null) {
+            log.info("User Disconnected: " + username);
+            Message chatMessage = new Message();
+            chatMessage.setType(MessageType.DISCONNECT);
+            chatMessage.setSender(username);
+            messagingTemplate.convertAndSend("/topic/public", chatMessage);
+        }
+    }
+}
