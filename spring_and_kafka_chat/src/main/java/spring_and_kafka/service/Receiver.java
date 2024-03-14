872f2e779261eb9bbb0c077c3527a30ea1826bd9
Vdym <vadimnaumnaum@gmail.com>
2024-03-14 09:44:48 +0100

    Created class  Receiver servicrce

---
+++ spring_and_kafka_chat/src/main/java/spring_and_kafka/service/Receiver.java
@@ -0,0 +1,35 @@
+package spring_and_kafka.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.user.SimpSession;
+import org.springframework.messaging.simp.user.SimpUser;
+import org.springframework.messaging.simp.user.SimpUserRegistry;
+import org.springframework.stereotype.Service;
+import spring_and_kafka.entity.Message;
+
+@Service
+@Slf4j
+public class Receiver {
+
+    private final SimpMessageSendingOperations messagingTemplate;
+    private final SimpUserRegistry userRegistry;
+
+    public Receiver(SimpMessageSendingOperations messagingTemplate, SimpUserRegistry userRegistry) {
+        this.messagingTemplate = messagingTemplate;
+        this.userRegistry = userRegistry;
+    }
+
+    @KafkaListener(topics = "messaging", groupId = "chat")
+    public void consume(Message chatMessage) {
+        log.info("Received message from Kafka: " + chatMessage);
+        for (SimpUser user : userRegistry.getUsers()) {
+            for (SimpSession session : user.getSessions()) {
+                if (!session.getId().equals(chatMessage.getSessionId())) {
+                    messagingTemplate.convertAndSendToUser(session.getId(), "/topic/public", chatMessage);
+                }
+            }
+        }
+    }
+}
