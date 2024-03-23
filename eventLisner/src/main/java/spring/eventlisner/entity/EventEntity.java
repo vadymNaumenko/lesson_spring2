1445ed6574c0ada5f8b026280c831bbd6f9b0da9
Vdym <vadimnaumnaum@gmail.com>
2024-03-23 22:26:09 +0100

    Updated eventEntity for db and added generic

--- eventLisner/src/main/java/spring/eventlisner/entity/EventEntity.java
+++ eventLisner/src/main/java/spring/eventlisner/entity/EventEntity.java
@@ -1,19 +1,24 @@
 package spring.eventlisner.entity;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
 import org.springframework.context.ApplicationEvent;
 
 @Getter
-@Setter
-@ToString
-public class EventEntity extends ApplicationEvent {
-    private final AccessType accessType;
-    private Integer id;
-
-    public EventEntity(Object source, AccessType accessType) {
+@Entity
+public class EventEntity<T> extends ApplicationEvent {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private AccessType accessType;
+    public EventEntity(T source, AccessType accessType) {
         super(source);
         this.accessType = accessType;
+    }
+    public EventEntity() {
+        super(null);
     }
 }
