a043b43475a9863d9d981195b929c10eaad83330
Vdym <vadimnaumnaum@gmail.com>
2024-04-07 12:47:56 +0200

    created cacheService with two methods

--- cache_spring/src/main/java/cache_spring/CacheService.java
+++ cache_spring/src/main/java/cache_spring/CacheService.java
@@ -1,7 +1,20 @@
 package cache_spring;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class CacheService {
+    private final Dao dao;
+
+    public String getData(){
+        return dao.getData();
+    }
+    public String deleteData(){
+    return "delete";
+    }
+
+
+
 }
