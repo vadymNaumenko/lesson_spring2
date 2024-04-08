424158cc70dc26c2e6998383d1ddf56c7c2d7e9c
Vdym <vadimnaumnaum@gmail.com>
2024-04-08 12:52:33 +0200

    created controller with one method getData

---
+++ cache_spring/src/main/java/cache_spring/CacheController.java
@@ -0,0 +1,18 @@
+package cache_spring;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class CacheController {
+
+    private final CacheService cacheService;
+    @GetMapping("/getData")
+    public String getData(){
+        return cacheService.getData();
+    }
+
+}
