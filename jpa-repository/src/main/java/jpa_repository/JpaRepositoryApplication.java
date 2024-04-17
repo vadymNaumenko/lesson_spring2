967ea3d6ceee04333b93ddf1ab2c21023ef40098
Vdym <vadimnaumnaum@gmail.com>
2024-04-17 22:45:18 +0200

    created controller

--- jpa-repository/src/main/java/jpa_repository/JpaRepositoryApplication.java
+++ jpa-repository/src/main/java/jpa_repository/JpaRepositoryApplication.java
@@ -1,15 +1,16 @@
 package jpa_repository;
 
+import jpa_repository.service.PostService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class JpaRepositoryApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JpaRepositoryApplication.class, args);
-
-        System.out.println();
     }
 
 }
