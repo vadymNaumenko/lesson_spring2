81c18ea0813842c498e1be7eae7c187cf67dc2f7
Vdym <vadimnaumnaum@gmail.com>
2024-04-18 23:18:44 +0200

    update query

--- jpa-repository/src/main/java/jpa_repository/repository/PostRepository.java
+++ jpa-repository/src/main/java/jpa_repository/repository/PostRepository.java
@@ -2,6 +2,7 @@ package jpa_repository.repository;
 
 import jpa_repository.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -9,16 +10,23 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface PostRepository extends JpaRepository<Post,Integer> {
+public interface PostRepository extends JpaRepository<Post, Integer> {
     @Query("select p from Post p where p.title = :title")
-    public Optional<Post> findByTitle(String title);
+    Optional<Post> findByTitle(String title);
 
-    public List<Post> findAllByTitleContainingAndDescriptionContaining(String title, String description);
+    List<Post> findAllByTitleContainingAndDescriptionContaining(String title, String description);
+
     @Query("select p from Post p" +
            " where p.title like %:title% and p.description like %:description%")
-    public List<Post> findAllBy(String title, String description);
+    List<Post> findAllBy(String title, String description);
+
+    @Modifying(clearAutomatically = true,flushAutomatically = true)
+    @Query("update Post p " +
+           "set p.title = :title " +
+           "where p.id in (:ids)")
+    int updateLocation(String title, Integer... ids);
 
     @Query("select p from Post p" +
            " join fetch p.locales pl where p.title = :title")
-    public Optional<Post> findByTitle2(String title);
+    Optional<Post> findByTitle2(String title);
 }
