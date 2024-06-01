c7bb8fe68e2f658ff9f0b8e2406136aa9d8c344d
Vdym <vadimnaumnaum@gmail.com>
2024-06-01 23:36:22 +0200

    created auth controller

--- test_starter/src/main/java/spring/controller/UserController.java
+++ test_starter/src/main/java/spring/controller/UserController.java
@@ -1,18 +1,31 @@
 package spring.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import spring.auth.AuthRequest;
+import spring.auth.AuthResponse;
+import spring.auth.AuthService;
+import spring.auth.RegistrationRequest;
 
 @Controller
+@RequiredArgsConstructor
 public class UserController {
 
-    @GetMapping
-    public String hallo(){
-        return "Halo";
+    private final AuthService service;
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> authenticate(@RequestBody AuthRequest request){
+        // todo if email not found make redirect and find by nickname
+        return ResponseEntity.ok(service.authenticate(request));
     }
-    @GetMapping("/vadym")
-    public String halloVadym(){
-        return "Halo Vadym";
+
+    @PostMapping("/registration")
+    public ResponseEntity<AuthResponse> registration(@RequestBody RegistrationRequest request){
+        return ResponseEntity.ok(service.register(request));
     }
     @GetMapping("/hallo/vadym")
     public String halloVadymas(){
